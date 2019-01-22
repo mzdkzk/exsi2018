1fbee71327267852f103638768f6d3be32d0e3a4
mzdkzk <mzdkzk@yahoo.co.jp>
2019-01-23 00:03:55 +0900

    提出用に修正

--- report.php
+++ report.php
@@ -1,8 +1,32 @@
 <?php
-  mb_internal_encoding("UTF-8");
   require_once("lib/phpQuery-onefile.php");
   require_once("lib/pathChanger.php");
-
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>平成31年度和歌山大学カレンダー</title>
+  <link rel="stylesheet" href="css/fullcalendar.min.css">
+</head>
+<body>
+<h1>平成31年度和歌山大学カレンダー</h1>
+<?php
+  $url = "http://www.wakayama-u.ac.jp/scenter/basic/calendar/calendar_h31.html";
+  $doc = phpQuery::newDocumentFileHtml($url);
+  echo "<p>";
+  echo sprintf("取得サイト:%s<br>", $doc["title"]->text());
+  echo sprintf("<a href='%s'>%s</a>", $url, $url);
+  echo "</p>";
+?>
+<div id='calendar'></div>
+<script src="js/jquery.min.js"></script>
+<script src="js/moment.min.js"></script>
+<script src="js/fullcalendar.min.js"></script>
+<script src="js/locale/ja.js"></script>
+<script>
+$(document).ready(function() {
+  <?php
   $url = "http://www.wakayama-u.ac.jp/scenter/basic/calendar/calendar_h31.html";
   $doc = phpQuery::newDocumentFileHtml($url);
 
@@ -44,5 +68,13 @@
     $result = array_merge($result, get_date($data));
   }
 
-  header("Content-Type: application/json; charset=UTF-8");
-  echo json_encode(["result" => $result, "length" => count($result)], JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+  $json = json_encode(["result" => $result, "length" => count($result)], JSON_UNESCAPED_UNICODE);
+  echo sprintf("let data = %s;", $json);
+  ?>
+  $('#calendar').fullCalendar({
+    events: data.result
+  })
+})
+</script>
+</body>
+</html>
